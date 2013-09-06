8431d4abf40666152e200c35b122ce18c76227ca
wadadaaa <lopatinski@gmail.com>
2013-09-06 14:18:48 +0300

    changes in wsgi

--- zakailtd/wsgi.py
+++ zakailtd/wsgi.py
@@ -15,11 +15,12 @@ framework.
 """
 import os
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zakailtd.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zakailtd.settings")
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+application = get_wsgi_application()
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
